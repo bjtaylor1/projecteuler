b2f433afb716834aacc5e95f1d8823088fce597e
Ben Taylor <ben.taylor@ssp-uk.com>
2016-12-29 11:05:54 +0000

    asymptotic with 3x3 square

--- euler575/euler575.cpp
+++ euler575/euler575.cpp
@@ -15,13 +15,13 @@ using namespace std;
 
 enum tiletype
 {
-	corner,middle,side
+	corner, middle, side
 };
 
 class tile_type
 {
 public:
-	stack<double> transfers;
+	vector<double> transfers;
 
 	double gives, current;
 	double weight;
@@ -31,11 +31,7 @@ public:
 
 	void apply_transfers()
 	{
-		while (!transfers.empty())
-		{
-			current -= transfers.top();
-			transfers.pop();
-		}
+
 
 	}
 };
@@ -47,8 +43,8 @@ public:
 	int x, y, i;
 	tile_type *tt_heads, *tt_tails;
 
-	tile(int _i, 
-		tile_type* tt_corner_heads, tile_type* tt_side_heads, tile_type* tt_middle_heads, 
+	tile(int _i,
+		tile_type* tt_corner_heads, tile_type* tt_side_heads, tile_type* tt_middle_heads,
 		tile_type* tt_corner_tails, tile_type* tt_side_tails, tile_type* tt_middle_tails) : i(_i)
 	{
 		x = _i % SIZE;
@@ -74,7 +70,7 @@ public:
 			tt_tails = tt_middle_tails;
 		}
 	}
-	
+
 	void add_neighbour(tile* neighbour)
 	{
 		tt_heads->neighbours.push_back(neighbour->tt_heads);
@@ -88,8 +84,8 @@ int main()
 {
 	vector<tile> tiles;
 	//make the tiles
-	tile_type	corner_h(corners, ONE/3, corner), side_h(sides, ONE/4, side), middle_h(middles, ONE/5, middle), 
-				corner_t(corners, ONE/4, corner), side_t(sides, ONE/6, side), middle_t(middles, ONE/8, middle);
+	tile_type	corner_h(corners, ONE / 3, corner), side_h(sides, ONE / 4, side), middle_h(middles, ONE / 5, middle),
+		corner_t(corners, ONE / 4, corner), side_t(sides, ONE / 6, side), middle_t(middles, ONE / 8, middle);
 
 	tile_type* tile_types[] = { &corner_t, &side_t, &middle_t };
 
@@ -117,34 +113,63 @@ int main()
 
 	}
 
-	for (int i = 0; i < 10; i++)
+	for (int tt = 0; tt < sizeof(tile_types) / sizeof(tile_type*); tt++)
 	{
+		for (vector<tile_type*>::iterator it = tile_types[tt]->neighbours.begin(); it != tile_types[tt]->neighbours.end(); it++)
+		{
+			double amountToGive = (*it)->gives;
+			tile_types[tt]->transfers.push_back(-amountToGive);
+			(*it)->transfers.push_back(amountToGive);
+		}
+	}
+
+
+
+	for (int i = 0; i < 1000; i++)
+	{
+		stack<pair<tile_type*, double>> gains;
+		stack<pair<tile_type*, double>> losses;
+
 		for (int tt = 0; tt < sizeof(tile_types) / sizeof(tile_type*); tt++)
 		{
+			double gainByThisTileType = 0;
 			for (vector<tile_type*>::iterator it = tile_types[tt]->neighbours.begin(); it != tile_types[tt]->neighbours.end(); it++)
 			{
-				double amountToGive = (*it)->gives * (*it)->current;
-				tile_types[tt]->transfers.push(-amountToGive) ;
-				(*it)->transfers.push(amountToGive );
+				double gainFromThisNeighbour = (*it)->gives * (*it)->current / (*it)->weight;
+				losses.push(pair<tile_type*, double>(*it, gainFromThisNeighbour));
+				gainByThisTileType += gainFromThisNeighbour;
 			}
+			gains.push(pair<tile_type*, double>(tile_types[tt], gainByThisTileType));
 		}
-		double checksum = 0;
-		for (int tt = 0; tt < sizeof(tile_types) / sizeof(tile_type*); tt++)
+		while (!gains.empty())
 		{
-			tile_types[tt]->apply_transfers();
-			//if (i % 10 == 0)
+			gains.top().first->current += gains.top().second;
+			gains.pop();
+		}
+		while (!losses.empty())
+		{
+			losses.top().first->current -= losses.top().second;
+			losses.pop();
+		}
+
+
+		double checksum = 0;
+
+
+		if (i % 10 == 0)
+		{
+			for (int tt = 0; tt < sizeof(tile_types) / sizeof(tile_type*); tt++)
 			{
 				if (tt > 0) cout << ",";
 				cout << fixed << setprecision(12) << tile_types[tt]->current;
 				checksum += tile_types[tt]->current;
 			}
+			cout << ", checksum = " << checksum << endl;
 		}
-		cout << ", checksum = " << checksum << endl;
-		
 	}
-	
-	
 
-    return 0;
+
+
+	return 0;
 }
 
