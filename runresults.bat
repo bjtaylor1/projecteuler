ee576d9d5cdadf75b21947a5373362f1212ee504
Ben Taylor <ben.taylor@ssp-uk.com>
2017-01-13 19:39:04 +0000

    add runresults.bat

---
+++ runresults.bat
@@ -0,0 +1,11 @@
+x64\release\euler579c 1500 50
+x64\release\euler579c 1550 50
+x64\release\euler579c 1600 50
+x64\release\euler579c 1650 50
+x64\release\euler579c 1700 50
+x64\release\euler579c 1750 50
+x64\release\euler579c 1800 50
+x64\release\euler579c 1850 50
+x64\release\euler579c 1900 50
+x64\release\euler579c 1950 50
+x64\release\euler579c 2000 50
