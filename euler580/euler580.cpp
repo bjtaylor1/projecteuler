99366f4e3d35002349219ca1d86595d82abc3fd9
Ben Taylor <ben.taylor@ssp-uk.com>
2017-02-05 14:49:51 +0000

    580 stores memory by k

--- euler580/euler580.cpp
+++ euler580/euler580.cpp
@@ -15,21 +15,26 @@ int main(int argc, char** argv)
 {
 	if (argc < 2) return 1;
 	long long limit = stoll(argv[1]);
-	bool* is_hs = new bool[limit];
-	memset(is_hs, 0, limit * sizeof(bool));
+	long long kmax = (limit - 1) / 4;
+	bool* is_hs = new bool[kmax];
+	memset(is_hs, 0, kmax * sizeof(bool));
 	long long h;
 	for (long long k = 1; (h = (4 * k + 1)) < limit; k++)
 	{
 		long long hs = h*h;
 		for (long long hsm = hs; hsm < limit; hsm += hs)
 		{
-			is_hs[hsm] = true;
+			if (((hsm - 1) % 4) == 0)
+			{
+				long long kofhsm = (hsm - 1) / 4;
+				is_hs[kofhsm] = true;
+			}
 		}
 	}
 	long long tot = 0;
 	for (long long k = 0; (h = (4 * k + 1)) < limit; k++)
 	{
-		if (!is_hs[h])
+		if (!is_hs[k])
 		{
 			tot++;
 		}
