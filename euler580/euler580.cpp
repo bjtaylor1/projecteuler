c1f797a9e00b37c87be1aee1434fdb4166d4d34f
Ben Taylor <ben.taylor@ssp-uk.com>
2017-02-05 14:55:40 +0000

    580 works with memmax with 1 partition

--- euler580/euler580.cpp
+++ euler580/euler580.cpp
@@ -16,27 +16,35 @@ int main(int argc, char** argv)
 	if (argc < 2) return 1;
 	long long limit = stoll(argv[1]);
 	long long kmax = (limit - 1) / 4;
-	bool* is_hs = new bool[kmax];
+#define MEMMAX 10000000
+
+	long partitions = ceil(((double)kmax) / MEMMAX);
+	bool* is_hs = new bool[MEMMAX];
 	memset(is_hs, 0, kmax * sizeof(bool));
-	long long h;
-	for (long long k = 1; (h = (4 * k + 1)) < limit; k++)
+	long long tot = 0;
+
+	for (long partition = 0; partition < partitions; partition++)
 	{
-		long long hs = h*h;
-		for (long long hsm = hs; hsm < limit; hsm += hs)
+		long long h, kmin = partition * MEMMAX;
+		for (long long k = kmin + 1; (h = (4 * k + 1)) < limit; k++)
 		{
-			if (((hsm - 1) % 4) == 0)
+			long long hs = h*h;
+			for (long long hsm = hs; hsm < limit; hsm += hs)
 			{
-				long long kofhsm = (hsm - 1) / 4;
-				is_hs[kofhsm] = true;
+				if (((hsm - 1) % 4) == 0)
+				{
+					long long kofhsm = ((hsm - 1) / 4) - kmin;
+					if (kofhsm >= MEMMAX) break;
+					is_hs[kofhsm] = true;
+				}
 			}
 		}
-	}
-	long long tot = 0;
-	for (long long k = 0; (h = (4 * k + 1)) < limit; k++)
-	{
-		if (!is_hs[k])
+		for (long long k = 0; k < MEMMAX && (h = (4 * k + 1)) < limit; k++)
 		{
-			tot++;
+			if (!is_hs[k])
+			{
+				tot++;
+			}
 		}
 	}
 	delete[] is_hs;
