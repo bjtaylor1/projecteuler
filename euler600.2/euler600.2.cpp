e15c3932b9b7c03b4614f83bbbf8d6da675d9b34
Ben Taylor <ben.taylor@ssp-uk.com>
2017-05-09 13:03:11 +0100

    parallelize 600v2

--- euler600.2/euler600.2.cpp
+++ euler600.2/euler600.2.cpp
@@ -11,18 +11,14 @@ long maxperim;
 mutex m_multivectors;
 double ANGLE_INCREMENT = M_PI / 3;
 
-#if _DEBUG
-long numthreads = 1;
-#else
-long numthreads = 8;
-#endif
+long numthreads = 0;
 
 class sidepair
 {
 public:
 	long s0, s1;
 	double x, y;
-	sidepair(long _s0, long _s1) : s0(_s0), s1(_s1), 
+	sidepair(long _s0, long _s1) : s0(_s0), s1(_s1),
 		x(_s0 * cos(ANGLE_INCREMENT * 4) + s1 * cos(ANGLE_INCREMENT * 5)),
 		y(_s0 * sin(ANGLE_INCREMENT * 4) + s1 * sin(ANGLE_INCREMENT * 5)) {}
 	sidepair(double _x, double _y) : x(_x), y(_y) {}
@@ -46,6 +42,71 @@ ostream& operator <<(ostream& os, const sidepair& sp)
 	return os;
 }
 
+atomic<unsigned long long> hexcount = 0;
+#if _DEBUG
+set<hexagon> hexagons;
+#endif
+set<sidepair> sidepairs;
+set<sidepair>::const_iterator currentsidepair;
+mutex m_currentsidepair;
+
+void process(const set<sidepair>::const_iterator v0)
+{
+	if (v0->s0 >= v0->s1)
+	{
+		long found = 0, duplicates = 0;
+		for (auto v1 = sidepairs.begin(); v1 != sidepairs.end(); v1++)
+		{
+			double totx =
+				v0->s0 * cos(ANGLE_INCREMENT * 0) +
+				v0->s1 * cos(ANGLE_INCREMENT * 1) +
+				v1->s0 * cos(ANGLE_INCREMENT * 2) +
+				v1->s1 * cos(ANGLE_INCREMENT * 3);
+			double toty =
+				v0->s0 * sin(ANGLE_INCREMENT * 0) +
+				v0->s1 * sin(ANGLE_INCREMENT * 1) +
+				v1->s0 * sin(ANGLE_INCREMENT * 2) +
+				v1->s1 * sin(ANGLE_INCREMENT * 3);
+			double distsq = totx*totx + toty*toty;
+			double perimleft = (maxperim - v0->s0 - v0->s1 - v1->s0 - v1->s1);
+			double perimleftsq = perimleft * perimleft;
+			if (distsq <= perimleftsq)
+			{
+				sidepair v2finder(-totx, -toty);
+
+				for (auto v2 = sidepairs.lower_bound(v2finder); v2 != sidepairs.end() && *v2 == v2finder; v2++)
+				{
+					vector<long> sides({ v0->s0, v0->s1, v1->s0, v1->s1, v2->s0, v2->s1 });
+					if (accumulate(sides.begin(), sides.end(), 0) <= maxperim)
+					{
+						hexagon h(sides);
+#if _DEBUG
+						hexagons.insert(h);
+#else
+						if (h.sides == h.sides_orig) hexcount++;
+						if (hexcount >= ULONG_MAX) throw runtime_error("Overflow");
+#endif
+					}
+				}
+			}
+
+		}
+	}
+}
+
+void do_processing()
+{
+	set<sidepair>::const_iterator sp;
+	do {
+		{
+			lock_guard<mutex> lm(m_currentsidepair);
+			if (currentsidepair == sidepairs.end()) return;
+			sp = currentsidepair++;
+		}
+		process(sp);
+	} while (sp != sidepairs.end());
+}
+
 int main(int argc, char** argv)
 {
 	{
@@ -55,8 +116,8 @@ int main(int argc, char** argv)
 	}
 
 	maxperim = stoi(argv[1]);
+	numthreads = stoi(argv[2]);
 
-	set<sidepair> sidepairs;
 	for (long s0 = 1; s0 <= (maxperim - 4) / 2; s0++)
 	{
 		long maxside = (maxperim / 2) - s0 - 1;
@@ -68,98 +129,36 @@ int main(int argc, char** argv)
 	}
 
 	cout << "Preprocessed " << sidepairs.size() << " side pairs." << endl;
+	currentsidepair = sidepairs.begin();
 
-	unsigned long long hexcount = 0;
-#if _DEBUG
-	set<hexagon> hexagons;
-#endif
-
-
-	for (auto v0 = sidepairs.begin(); v0 != sidepairs.end(); v0++)
+	vector<thread> threads;
+	for (long i = 0; i < numthreads; i++)
 	{
-		if (v0->s0 >= v0->s1)
-		{
-			long found = 0, duplicates = 0;
-			for (auto v1 = sidepairs.begin(); v1 != sidepairs.end(); v1++)
-			{
-				double totx =
-					v0->s0 * cos(ANGLE_INCREMENT * 0) +
-					v0->s1 * cos(ANGLE_INCREMENT * 1) +
-					v1->s0 * cos(ANGLE_INCREMENT * 2) +
-					v1->s1 * cos(ANGLE_INCREMENT * 3);
-				double toty =
-					v0->s0 * sin(ANGLE_INCREMENT * 0) +
-					v0->s1 * sin(ANGLE_INCREMENT * 1) +
-					v1->s0 * sin(ANGLE_INCREMENT * 2) +
-					v1->s1 * sin(ANGLE_INCREMENT * 3);
-				double distsq = totx*totx + toty*toty;
-				double perimleft = (maxperim - v0->s0 - v0->s1 - v1->s0 - v1->s1);
-				double perimleftsq = perimleft * perimleft;
-				if (distsq <= perimleftsq)
-				{
-					sidepair v2finder(-totx, -toty);
-
-					for (auto v2 = sidepairs.lower_bound(v2finder); v2 != sidepairs.end() && *v2 == v2finder; v2++)
-					{
-						vector<long> sides({ v0->s0, v0->s1, v1->s0, v1->s1, v2->s0, v2->s1 });
-						if (accumulate(sides.begin(), sides.end(), 0) <= maxperim)
-						{
-							hexagon h(sides);
-#if _DEBUG
-							auto inserted = hexagons.insert(h);
-							if (!inserted.second)
-							{
-								//cout << h << " is a duplicate of " << *inserted.first << endl;
-								duplicates++;
-							}
-							else
-							{
-								//cout << h << endl;
-								found++;
-							}
-							found++;
-#else
-							if (h.sides == h.sides_orig) hexcount++;
-							if (hexcount >= ULONG_MAX) throw runtime_error("Overflow");
-#endif
-						}
-					}
-				}
-
-				//cout << v0->s0 << "," << v0->s1 << "," << v1->s0 << "," << v1->s1 << ": found " << found << ", of which " << duplicates << " are duplicates" << endl;
-
-
-
-			}
-
-			if (found == 0)
-			{
-				cout << "None found for " << *v0 << endl;
-			}
-			else
-			{
-				cout << *v0 << " - found " << found << endl;
-			}
-		}
+		threads.push_back(thread(do_processing));
+	}
+	for (vector<thread>::iterator t = threads.begin(); t != threads.end(); t++)
+	{
+		t->join();
 	}
 
-//#if _DEBUG
-//	for (auto h : hexagons)
-//	{
-//		cout << h << endl;
-//	}
-//	cout << "There are " << hexagons.size() << " hexagons." << endl;
-//#else
-//	cout << "There are " << hexcount << " hexagons." << endl;
-//#endif
-	
+
+#if _DEBUG
+	for (auto h : hexagons)
+	{
+		cout << h << endl;
+	}
+	cout << "There are " << hexagons.size() << " hexagons." << endl;
+#else
+	cout << "There are " << hexcount << " hexagons." << endl;
+#endif
+
 	{
 		auto now = std::chrono::system_clock::now();
 		auto now_c = std::chrono::system_clock::to_time_t(now);
 		std::cout << "end: " << std::put_time(std::localtime(&now_c), "%c") << endl;
 	}
 
-    return 0;
+	return 0;
 }
 
 
