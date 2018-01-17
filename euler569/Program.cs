50e08552af2444a180fb95ee7902d0484f519d4f
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-17 14:05:21 +0000

    merge loops

--- euler569/Program.cs
+++ euler569/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -12,30 +13,16 @@ namespace euler569
         static void Main(string[] args)
         {
             const int limit = 2500000;
+
             mpz_t prime = 2;
             int totalCount = 0;
-            var peaks = new Dictionary<int, Pos>(limit)
+            var peaks = new Dictionary<int, Pos>()
             {
                 [0] = new Pos(0, 0, 0),
                 [1] = new Pos(2, 2, 1)
             };
             Stopwatch sw = Stopwatch.StartNew();
-            for(int i = 2; i <= limit; i++)
-            {
-                if (i % 10000 == 0)
-                {
-                    var velocity = (1000000000d) / sw.ElapsedTicks;
-                    Console.Write($"{(double)i / limit:P1}, velocity={velocity:0.00000}\r");
-                    sw.Restart();
-                }
-                var next2Primes = (prime = prime.NextPrimeGMP(), prime = prime.NextPrimeGMP());
-                var xpos = peaks[i-1].X + next2Primes.Item1 + next2Primes.Item2;
-                var ypos = peaks[i-1].Y - next2Primes.Item1 + next2Primes.Item2;
-                peaks[i] = new Pos(xpos, ypos, i);
-            }
-            Console.WriteLine("\nMade peaks");
 
-            int lowerLimit = 1;
             for (int i = 2; i <= limit; i++)
             {
                 if (i % 10000 == 0)
@@ -44,9 +31,20 @@ namespace euler569
                     Console.Write($"{(double)i / limit:P1}, velocity={velocity:0.00000}\r");
                     sw.Restart();
                 }
+                var next2Primes = (prime = prime.NextPrimeGMP(), prime = prime.NextPrimeGMP());
+                var xpos = peaks[i - 1].X + next2Primes.Item1 + next2Primes.Item2;
+                var ypos = peaks[i - 1].Y - next2Primes.Item1 + next2Primes.Item2;
+                peaks[i] = new Pos(xpos, ypos, i);
+            }
+            Console.WriteLine("\nMade peaks");
 
-                Fraction minAngle = Fraction.AngleBetween(peaks[i], peaks[i-1]);
+            int lowerLimit = 1;
+            peaks[2].IsHull = true;
+            for (int i = 2; ; i++)
+            {
+                Console.WriteLine($"{lowerLimit} - {i}, {(double)i / limit:P1}");
                 int count = 1;
+                Fraction minAngle = Fraction.AngleBetween(peaks[i], peaks[i - 1]);
                 for (int j = i - 2; j >= lowerLimit; j--)
                 {
                     var angle = Fraction.AngleBetween(peaks[i], peaks[j]);
@@ -56,16 +54,18 @@ namespace euler569
                         count++;
                     }
                 }
-                if (peaks[i].Previous > lowerLimit)
-                {
-                    lowerLimit = peaks[i].Previous;
-                    peaks = peaks.Where(p => p.Key >= lowerLimit).ToDictionary(k => k.Key, k => k.Value);
-                }
-
-
                 totalCount += count;
-                if (peaks[i].Previous > lowerLimit) lowerLimit = peaks[i].Previous;
+                if (i == limit) break;
+                if(peaks[i].IsHull)
+                {
+                    var next = peaks.Where(p => p.Key > i)
+                          .OrderByDescending(p => Fraction.AngleBetween(p.Value, peaks[i]))
+                          .First();
+                    next.Value.IsHull = true;
+                    lowerLimit = i;
+                }                
             }
+
             Console.WriteLine($"\n{totalCount}");
         }
     }
@@ -88,6 +88,7 @@ namespace euler569
         public mpz_t Y { get; }
         public int Id { get; }
         public int Next { get; set; }
+        public bool IsHull { get; internal set; }
     }
 
     public class Fraction : IComparable<Fraction>
---
+++ euler619/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler619
+{
+    class Program
+    {
+        int[] factors(int n)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
