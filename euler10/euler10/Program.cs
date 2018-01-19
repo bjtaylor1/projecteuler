ac289040347a2df14219c960bc4bf0bffbe130a9
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-19 12:42:20 +0000

    10 and (start) 27

---
+++ euler10/euler10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mpir.NET;
+
+namespace euler10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tot = new mpz_t(0);
+            for(mpz_t p = 2; p < 2e6; p = p.NextPrimeGMP())
+            {
+                tot += p;
+            }
+            Console.WriteLine(tot);
+        }
+    }
+}
