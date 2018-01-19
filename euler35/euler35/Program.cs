db4c6eb7f7126a1566b75ab9065b52ea5073ffce
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-19 13:15:07 +0000

    35 (optimize)

--- euler35/euler35/Program.cs
+++ euler35/euler35/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Mpir.NET;
 
 namespace euler35
 {
     class Program
     {
+        static readonly Regex telltaleChars = new Regex("[024568]", RegexOptions.Compiled);
         static bool IsCircular(mpz_t p)
         {
             string ps = p.ToString();
+            if (ps.Length > 1 && telltaleChars.IsMatch(ps))
+                return false;
             for (int i = 1; i < ps.Length; i++)
             {
                 string c = $"{ps.Substring(i)}{ps.Substring(0, i)}";
@@ -21,11 +26,13 @@ namespace euler35
         static void Main(string[] args)
         {
             int count = 0;
+            var sw = Stopwatch.StartNew();
             for(mpz_t p = 2; p < 1e6; p =p.NextPrimeGMP())
             {
                 if (IsCircular(p)) count++;
             }
-            Console.WriteLine(count);
+            sw.Stop();
+            Console.WriteLine($"{count}, in {sw.Elapsed}");
         }
     }
 }
