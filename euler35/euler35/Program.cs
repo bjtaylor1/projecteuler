a11de5a6de4787c25167c149b981373a658fcb3a
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-19 13:10:51 +0000

    35

---
+++ euler35/euler35/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Mpir.NET;
+
+namespace euler35
+{
+    class Program
+    {
+        static bool IsCircular(mpz_t p)
+        {
+            string ps = p.ToString();
+            for (int i = 1; i < ps.Length; i++)
+            {
+                string c = $"{ps.Substring(i)}{ps.Substring(0, i)}";
+                var pr = new mpz_t(c);
+                if (!pr.IsProbablyPrimeRabinMiller(10))
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for(mpz_t p = 2; p < 1e6; p =p.NextPrimeGMP())
+            {
+                if (IsCircular(p)) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
