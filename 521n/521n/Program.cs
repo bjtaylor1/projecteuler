dc182fafe3c74263c42e31ea639e92318c648c6b
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-22 16:26:04 +0000

    521n bf debugging

--- 521n/521n/Program.cs
+++ 521n/521n/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mpir.NET;
 
 namespace _521n
@@ -15,9 +13,26 @@ namespace _521n
                 if (n % p == 0) return (int) p;
         }
 
+        static mpz_t S(mpz_t n)
+        {
+            mpz_t result = new mpz_t(0);
+            for (mpz_t p = 2; p <= n; p = p.NextPrimeGMP())
+            {
+                
+            }
+            return result;
+        }
+
         static mpz_t S_bf(int n)
         {
-            return Enumerable.Range(2, n - 1).ToArray().Sum(i => smpf(i));
+            var dict = new Dictionary<int,int>();
+            for (int i = 2; i <= n; i++)
+            {
+                dict[i] = smpf(i);
+            }
+            var res = dict.Values.Sum();
+            var groups = dict.GroupBy(k => k.Value);
+            return res;
         }
         static void Main(string[] args)
         {
