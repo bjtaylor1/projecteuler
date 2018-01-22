0dd6a68b1edb43a72427c45ff9387df4d4b1f911
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-22 14:48:53 +0000

    480n async

--- euler480n/euler480n/Program.cs
+++ euler480n/euler480n/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace euler480n
 {
