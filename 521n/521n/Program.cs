93b68dbfd9c48bf41c3c74dec476c2e6505acc45
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-22 16:13:21 +0000

    521n bf

---
+++ 521n/521n/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mpir.NET;
+
+namespace _521n
+{
+    class Program
+    {
+        static int smpf(mpz_t n)
+        {
+            for (mpz_t p = 2; ; p = p.NextPrimeGMP())
+                if (n % p == 0) return (int) p;
+        }
+
+        static mpz_t S_bf(int n)
+        {
+            return Enumerable.Range(2, n - 1).ToArray().Sum(i => smpf(i));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(S_bf(100));
+        }
+    }
+}
