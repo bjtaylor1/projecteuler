afc942b17ae5d6edaf8051642e313ddd069f6cda
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-24 20:04:40 +0000

    521.py

--- euler521.py
+++ euler521.py
@@ -1,20 +1,35 @@
+from datetime import datetime
 import primes
-from operator import mul
+
+cache = dict()
 
 def Cp(n,p,prod,primes):
-    cp = n//prod
-    lowerprimes = list(filter(lambda op : op < p, primes))
-    if lowerprimes:
-        subtractions = {po: Cp(n,po,prod*po,lowerprimes) for po in lowerprimes}
-        cp -= sum(subtractions.itervalues())
-    return cp
+    if p in cache:
+        print("cache hit!")
+        return cache[p]
+    
+    cp = 0
+    subt=0
+    if prod <= n:
+        cp = n//prod
+        lowerprimes = list(filter(lambda op : op < p, primes))
+        if lowerprimes:
+            subtractions = {po: Cp(n,po,prod*po,lowerprimes) for po in lowerprimes}
+            subt += sum(subtractions.itervalues())
+    cache[p]=cp
+    return cp-subt
 
 def S(n):
     r = int(n**0.5)
     primestoroot = list(primes.values(r))
-    tot = sum(p * Cp(n,p,p,primestoroot) for p in primestoroot ) \
-        + primes.sum(n) - primes.sum(r)
-    
+    print(str(datetime.now()) + " made primestoroot")
+    tot = 0
+    tot += sum(p * Cp(n,p,p,primestoroot) for p in primestoroot )
+    print(str(datetime.now()) + " summed up primestoroot's smpf")
+    tot += primes.sum(n)
+    print("Summed up higher primes " + str(datetime.now()))
+    tot -=sum(primestoroot)
     return tot
 
-print(S(10**12)%(10**9))
+print(S(100))
+#print(S(10**12)%(10**9))
