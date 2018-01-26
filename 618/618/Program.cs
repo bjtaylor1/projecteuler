504fa742a2cab37556eda01877aa768d80108211
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-26 16:36:48 +0000

    618 cache attempt. doesn't work fast enough

--- 618/618/Program.cs
+++ 618/618/Program.cs
@@ -4,12 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mpir.NET;
 
 namespace _618
 {
     class Program
     {
-        private static int[] primes = new[] { 2, 3, 5, 7 };
+        const int N = 24;
+        private static int[] fib = new int[N+1];
+        private static int[] primes;
+        static Program()
+        {
+            fib[1] = 1;
+            for(int i = 2; i <= N; i++ )
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            var primesList = new List<mpz_t>();
+            for(mpz_t p = 2; p <= fib.Last(); p = p.NextPrimeGMP())
+            {
+                primesList.Add(p);
+            }
+            primes = primesList.Select(i => (int)i).ToArray(); //it should fit
+        }
+
         private static ConcurrentDictionary<int, int[]> cache = new ConcurrentDictionary<int, int[]>();
         static int[] S(int n)
         {
@@ -21,8 +39,8 @@ namespace _618
         {
             if (n == 0) return new int[] { };
             IEnumerable<int> subprimes = primes.Where(p => p <= n).ToArray();
-
-            int[] values = subprimes.SelectMany(p =>
+            Console.WriteLine($"Calculating {n}");
+            var values = subprimes.SelectMany(p =>
             {
                 if (p == n) return new int[] { p };
                 else
@@ -35,18 +53,20 @@ namespace _618
                     }).ToArray();
                     return res;
                 }
-            }).ToArray();
-            
-            var result = values.Where(v => v > 0).Distinct().ToArray();
+            }).ToList();
+            if (primes.Contains(n)) values.Add(n);
+
+            var allValues = values.Where(v => v > 0).ToArray();
+            var result = allValues.Distinct().ToArray();
             return result;
         }
 
         static void Main(string[] args)
         {
             int max = 0;
-            var s8 = S(8);
-
-            Console.WriteLine(s8);
+            var s8 = S(1000);
+            Console.WriteLine("Calculated");
+            Console.WriteLine(s8.Sum());
         }
     }
 
