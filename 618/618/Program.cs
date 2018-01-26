bbd47776a51d19ede353721f8c2d540110113b92
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-26 15:42:24 +0000

    618

---
+++ 618/618/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _618
+{
+    class Program
+    {
+        private static int[] primes = new[] { 2, 3, 5, 7 };
+        private static ConcurrentDictionary<int, int[][]> cache = new ConcurrentDictionary<int, int[][]>();
+        static int[][] S(int n)
+        {
+            var sum = cache.GetOrAdd(n, CalcN);
+            return sum;
+        }
+
+        private static int[][] CalcN(int n)
+        {
+            if (n == 0) return new int[][] { };
+            IEnumerable<int> subprimes = primes.Where(p => p <= n).ToArray();
+
+            int[][] values = subprimes.SelectMany(p =>
+            {
+                if (p == n) return new int[][] { new[] { p } };
+                else
+                {
+                    var n1 = n;
+                    int[][] sLower = S(n - p);
+                    int[][] res = sLower.Select(s =>
+                    {
+                        var n2 = n;
+                        var r = s.Concat(new[] { p }).OrderBy(i => i).ToArray();
+                        return r;
+                    }).ToArray();
+                    return res;
+                }
+            }).ToArray();
+            
+            var result = values.Where(v => v.Any()).Distinct(SequenceEqualityComparer.Instance).ToArray();
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int max = 0;
+            var s8 = S(8);
+
+            Console.WriteLine();
+        }
+    }
+
+    public class SequenceEqualityComparer : IEqualityComparer<int[]>
+    {
+        public static SequenceEqualityComparer Instance => new SequenceEqualityComparer();
+        private SequenceEqualityComparer()
+        {
+        }
+
+        public bool Equals(int[] x, int[] y)
+        {
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(int[] obj)
+        {
+            unchecked { return obj.Aggregate(397, (st, i) => (st * 397) ^ i.GetHashCode()); }
+        }
+    }
+}
--- primes.py
+++ primes.py
@@ -2,7 +2,7 @@ import math
 
 def values(x):
     result=[False,True,True]+[True,False]*((x+1)//2)
-    for i in range(3, math.ceil(x**0.5), 2):
+    for i in range(3, int(math.ceil(x**0.5)), 2):
         if result[i]:
             for pr in range(i*2, x+1, i):
                 result[pr] = False
