d9b367c13bb51a216042c47e24bf63b43176f6fb
Ben Taylor <bentaylortheonly@gmail.com>
2018-01-30 13:40:44 +0000

    slightly optimize 31

--- 0031.py
+++ 0031.py
@@ -1,14 +1,13 @@
 coins=[1,2,5,10,20,50,100,200]
-def W(c, n):
+def W(ci, n):
     if n==0:
         return 1
     
-    if len(c)==0 or n <= 0:
+    if ci < 0 or n <= 0:
         return 0
         
-    cw=c[:]
-    last=cw.pop()
-    res=W(cw, n) + W(c,n-last) #ways excluding last, + ways including last
+    last=coins[ci]
+    res=W(ci - 1, n) + W(ci, n-last) #ways excluding last, + ways including last
     return res
     
-print(W(coins,200))
+print(W(len(coins)-1,200))
