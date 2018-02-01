f86558937c33ff369408afed88410106b491e368
Ben Taylor <bentaylortheonly@gmail.com>
2018-02-01 15:15:44 +0000

    114 and 115

--- 0114.py
+++ 0114.py
@@ -1,4 +1,5 @@
-N=50
+import sys
+N=int(sys.argv[1])
 wr=[0]*(N+1)
 wb=[0]*(N+1)
 wb[1]=1
@@ -6,8 +7,8 @@ wb[2]=1
 wb[3]=1
 wr[3]=1
 for n in range(4,N+1):
-    wb[n]=wb[n-1]+1
-    wr[n]=wr[n-1]+1+wb[n-4]+1
+    wb[n]=wb[n-1]+wr[n-1]
+    wr[n]=wb[n-3]+wr[n-1]
     
 ans=wr[N]+wb[N]
 print(ans)
--- 0115.py
+++ 0115.py
@@ -1,16 +1,11 @@
-m=10
-wr=[0,0,0,1]
-wb=[0,1,1,1]
-n=3
+m=50
+wr=[0]*m+[1]
+wb=[0]+[1]*m
+n=m
 while(True):
     n+=1
     wb.append(wb[n-1]+wr[n-1])
     wr.append((wb[n-m] if n >= m else 0) +wr[n-1])
-    if(n==56):
-        print(wr[n]+wb[n])
-    if(n==57):
-        print(wr[n]+wb[n])
-        break
         
     if(wr[n] + wb[n] > 10**6):
         print(n)
