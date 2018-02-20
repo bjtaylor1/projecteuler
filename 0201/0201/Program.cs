81378566c12bd37aaac63bdcec46ce93bd956b1c
Ben Taylor <bentaylortheonly@gmail.com>
2018-02-20 17:09:16 +0000

    201 use memorycache gets to 75%

--- 0201/0201/Program.cs
+++ 0201/0201/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Linq;
+using System.Runtime.Caching;
+using System.Threading;
+using System.Threading.Tasks;
 using Mpir.NET;
 
 namespace _0201
@@ -13,45 +15,42 @@ namespace _0201
             else return max;
         }
 
+        static CacheItemPolicy cap = new CacheItemPolicy();
+
         const int usage = 50;
         //const int usage = 3;
         static int[] set = Enumerable.Range(1, 100).Select(i => i * i).ToArray();
         //static int[] set = new[] { 1, 3, 6, 8, 10, 11 };
-        static ConcurrentDictionary<(int tot, int max, int left), mpz_t> cache = new ConcurrentDictionary<(int tot, int max, int left), mpz_t>();
-        static mpz_t Ways(int tot, int max, int left) => cache.GetOrAdd((tot, NormalizeMax(max), left), Ways);
         static mpz_t Ways(int tot) => Ways(tot, set.Length - 1, usage);
-        static mpz_t Ways((int tot, int max, int left) p)
+        static mpz_t Ways(int tot, int max, int left)
         {
-            if (set.Take(p.max + 1).Reverse().Take(p.left).Sum() < p.tot) return 0;
-            if (p.left > usage) return 0;
-            if (p.tot < 0 || p.max < -1) return 0;
-            if (p.tot == 0) return p.left == 0 ? 1 : 0;
+            var cacheKey = $"{tot},{max},{left}";
+            var existing = MemoryCache.Default.Get(cacheKey);
+            if (existing != null) return (mpz_t)existing;
 
-            mpz_t with = p.max >= 0 ? cache.GetOrAdd((p.tot - set[p.max], p.max - 1, p.left - 1), Ways) : 0;
-            mpz_t without = cache.GetOrAdd((p.tot, p.max - 1, p.left), Ways);
+            if (set.Take(max + 1).Reverse().Take(left).Sum() < tot) return 0;
+            if (left > usage) return 0;
+            if (tot < 0 || max < -1) return 0;
+            if (tot == 0) return left == 0 ? 1 : 0;
+
+            mpz_t with = max >= 0 ? Ways(tot - set[max], max - 1, left - 1) : 0;
+            mpz_t without = Ways(tot, max - 1, left);
+            MemoryCache.Default.Set(cacheKey, with + without, cap);
             return with + without;
         }
         static void Main(string[] args)
         {
-
             var minSum = set.Take(usage).Sum();
             var maxSum = set.Reverse().Take(usage).Sum();
             Console.WriteLine($"Sum diff = {(maxSum - minSum)}");
             Console.WriteLine($"W({minSum}) = {Ways(minSum)}");
             Console.WriteLine($"W({maxSum}) = {Ways(maxSum)}");
-            mpz_t tot = 0;
-            for (int sum = minSum; sum <= maxSum; sum++)
+            long tot = 0;
+            Parallel.For(minSum, maxSum + 1, sum =>
             {
-                if(cache.Count > 30000000)
-                {
-                    Console.WriteLine("\nClearing!");
-                    cache.Clear();
-                    GC.Collect();
-                    GC.WaitForFullGCComplete();
-                }
-                Console.Write($"{((double)(sum - minSum) / (maxSum - minSum)):P1} {sum} {cache.Count}\r");
-                if (Ways(sum) == 1) tot += sum;
-            }
+                if (sum % 100 == 0) Console.Write($"{((double)(sum - minSum) / (maxSum - minSum)):P1} {sum} {MemoryCache.Default.GetCount()}\r");
+                if (Ways(sum) == 1) Interlocked.Add(ref tot, sum);
+            });
             Console.WriteLine(tot);
         }
     }
