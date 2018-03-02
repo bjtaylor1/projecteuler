8205ebb47dda206a731b6e4a48ac3581c0917943
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-02 15:20:06 +0000

    209 works

--- 0209cs/0209cs/Program.cs
+++ 0209cs/0209cs/Program.cs
@@ -1,63 +1,79 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EqualityComparers;
 
 namespace _0209cs
 {
     class Program
     {
+        const int bits = 6;
+        const long numBits = (1 << bits);
+        const long mask = numBits - 1;
+        static ConcurrentDictionary<(long, bool, bool), long> cache = new ConcurrentDictionary<(long, bool, bool), long>();
+
+        static long CountMatchingBitPatterns(long numBits, bool firstBitOfAll, bool firstBit)
+        {
+            return cache.GetOrAdd((numBits, firstBitOfAll, firstBit), CountMatchingBitPatterns);
+        }
+
+        static long CountMatchingBitPatterns((long numBits, bool firstBitOfAll, bool firstBit) p)
+        {
+            long retval;
+            if (p.numBits == 1)
+            {
+                retval = p.firstBit && p.firstBitOfAll ? 0 : 1;
+            }
+            else
+            {
+                retval = 0;
+                retval += CountMatchingBitPatterns(p.numBits - 1, p.firstBitOfAll, !p.firstBit);
+                if (!p.firstBit)
+                {
+                    retval += CountMatchingBitPatterns(p.numBits - 1, p.firstBitOfAll, p.firstBit);
+                }
+            }
+            return retval;
+        }
+
+        static long CountMatchingBitPatterns(long numBits)
+        {
+            long retval =
+                CountMatchingBitPatterns(numBits, false, false) +
+                CountMatchingBitPatterns(numBits, true, true);
+            return retval;
+        }
+
+
+        static long NextBitInSequence(long n)
+        {
+            long a = (n >> (bits - 1)) & 1;
+            long b = (n >> (bits - 2)) & 1;
+            long c = (n >> (bits - 3)) & 1;
+            long next = ((n << 1) & mask) | (a ^ (b & c));
+            return next;
+        }
+
         static void Main(string[] args)
         {
-            long res = 0;
-            int[] bitpos1 = new[] { 0, 1, 2, 3, 4, 5, 6, 7 };
-            int[] bitpos2 = new[] { 0, 2, 4, 7, 1, 3, 5, 6 };
-            for(long l = 0; l < 256; l++)
+            var bitSequences = new List<List<long>>();
+            for(long i = 0; bitSequences.Sum(b => b.Count) < numBits; i++)
             {
-                long[] bits1 = bitpos1.Select(b => (l >> b) & 1).ToArray();
-                long[] bits2 = bitpos2.Select(b => (l >> b) & 1).ToArray();
-                bool isMatch = true;
-                for (long i = 0; i < bitpos1.Length; i++)
+                if(!bitSequences.Any(l => l.Contains(i)))
                 {
-                    if ((bits1[i] & bits2[i]) != 0)
+                    var bitSequence = new List<long>();
+                    for(long n = i; !bitSequence.Contains(n); n = NextBitInSequence(n))
                     {
-                        isMatch = false;
-                        break;
+                        bitSequence.Add(n);
                     }
+                    bitSequences.Add(bitSequence);
                 }
-                if (isMatch) res++;
             }
-            Console.WriteLine(res);
-        }
-    }
 
-    public class Exclusion
-    {
-        public string Exclude { get; set; }
-        public string Reinclude { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Exclusion exclusion &&
-                   Exclude == exclusion.Exclude &&
-                   Reinclude == exclusion.Reinclude;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = 119986229;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Exclude);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Reinclude);
-            return hashCode;
-        }
-
-        public override string ToString()
-        {
-            return $"{Exclude} - {Reinclude}";
+            var bitSequenceLengths = bitSequences.Select(b => b.Count);
+            var bitsInSequence = bitSequenceLengths.Select(b => CountMatchingBitPatterns(b));
+            var totalCombinations = bitsInSequence.Aggregate(1L, (p, e) => p * e);
+            Console.WriteLine(totalCombinations);
         }
     }
 }
