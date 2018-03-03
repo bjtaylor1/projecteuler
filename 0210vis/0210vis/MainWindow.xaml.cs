f016b9bfc0fd7c4ad56280306eb0934b30998f4d
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-03 10:55:08 +0000

    0210vis renders points

--- 0210vis/0210vis/MainWindow.xaml.cs
+++ 0210vis/0210vis/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -17,50 +18,71 @@ namespace _0210vis
 
         }
 
-        int xmax = 24;
-        int ymax = 24;
+        int xmax = 4;
+        int ymax = 4;
         protected override void OnRender(DrawingContext drawingContext)
         {
-            Debug.WriteLine($"Rendering with size {ActualWidth}");
             c.Children.Clear();
             for (int x = -xmax; x <= xmax; x++)
             {
-                AddLine(Colors.LightGray, 1, x, x, -ymax, ymax);
+                AddLine(Brushes.LightGray, 1, x, -ymax, x, ymax);
             }
             for (int y = -ymax; y <= ymax; y++)
             {
-                AddLine(Colors.LightGray, 1, -xmax, xmax, y, y);
+                AddLine(Brushes.LightGray, 1, -xmax, y, xmax, y);
             }
-            AddLine(Colors.Black, 2, 0, 0, -ymax, ymax); //x axis
-            AddLine(Colors.Black, 2, -xmax, xmax, 0, 0); //y axis
+            AddLine(Brushes.Black, 2, 0, -ymax, 0, ymax); //x axis
+            AddLine(Brushes.Black, 2, -xmax, 0, xmax, 0); //y axis
 
+
+            AddLine(Brushes.LightBlue, 1, -xmax, 0, 0, ymax);
+            AddLine(Brushes.LightBlue, 1, 0, ymax, xmax, 0);
+            AddLine(Brushes.LightBlue, 1, xmax, 0, 0, -ymax);
+            AddLine(Brushes.LightBlue, 1, 0, -ymax, -xmax, 0);
+
+            AddPoint(Brushes.Red, 0, 0);
+            AddPoint(Brushes.Red, xmax / 4, ymax / 4);
         }
 
-        private void AddLine(Color color, int stroke, int x1, int x2, int y1, int y2)
+        private void AddPoint(Brush fill, int x, int y)
+        {
+            double size = Math.Min(ActualHeight, ActualWidth);
+            double lineX = size / (2 * xmax);
+            double lineY = size / (2 * ymax);
+            double shapeSize = Math.Min(lineX, lineY)/2;
+            var ellipse = new Ellipse();
+            ellipse.Height = shapeSize;
+            ellipse.Width = shapeSize;
+            ellipse.Fill = fill;
+            Canvas.SetLeft(ellipse, XPos(x) - shapeSize / 2);
+            Canvas.SetTop(ellipse, YPos(y) - shapeSize / 2);
+            c.Children.Add(ellipse);
+        }
+
+        private void AddLine(Brush stroke, int strokeThickness, int x1, int y1, int x2, int y2)
         {
             var line = new Line();
-            line.Stroke = new SolidColorBrush(color);
-            line.StrokeThickness = stroke;
+            line.Stroke = stroke;
+            line.StrokeThickness = strokeThickness;
             line.X1 = XPos(x1);
             line.X2 = XPos(x2);
             line.Y1 = YPos(y1);
             line.Y2 = YPos(y2);
-            Debug.WriteLine($"Adding line from {line.X1},{line.Y1} to {line.X2},{line.Y2}");
             c.Children.Add(line);
         }
 
         private double XPos(int x)
         {
-            double size = Math.Min(ActualHeight, ActualWidth);
-            double center = ActualWidth / 2;
+            double size = Math.Min(c.ActualHeight, c.ActualWidth);
+            double center = c.ActualWidth / 2;
             double pos = center + ((x * size) / (2 * xmax));
             return pos;
         }
 
         private double YPos(int y)
         {
-            double size = Math.Min(ActualHeight, ActualWidth);
-            double center = ActualHeight / 2;
+            double size = Math.Min(c.ActualHeight, c.ActualWidth);
+            double center = c.ActualHeight / 2;
             double pos = center - ((y * size) / (2 * ymax));
             return pos;
         }
