e5de7458279d816396473562dc403e1c27bd52bd
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-03 10:38:03 +0000

    0210vis renders axes

--- 0210vis/0210vis/MainWindow.xaml.cs
+++ 0210vis/0210vis/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _0210vis
@@ -23,46 +14,54 @@ namespace _0210vis
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         int xmax = 24;
         int ymax = 24;
-
         protected override void OnRender(DrawingContext drawingContext)
         {
-
-            base.OnRender(drawingContext);
-            
+            Debug.WriteLine($"Rendering with size {ActualWidth}");
             c.Children.Clear();
-            var yAxis = new Line();
-            Color color = Colors.Black;
-            const int stroke = 2;
-            AddLine(yAxis, color, stroke);
+            for (int x = -xmax; x <= xmax; x++)
+            {
+                AddLine(Colors.LightGray, 1, x, x, -ymax, ymax);
+            }
+            for (int y = -ymax; y <= ymax; y++)
+            {
+                AddLine(Colors.LightGray, 1, -xmax, xmax, y, y);
+            }
+            AddLine(Colors.Black, 2, 0, 0, -ymax, ymax); //x axis
+            AddLine(Colors.Black, 2, -xmax, xmax, 0, 0); //y axis
+
         }
 
-        private void AddLine(Line yAxis, Color color, int stroke)
+        private void AddLine(Color color, int stroke, int x1, int x2, int y1, int y2)
         {
-            yAxis.Stroke = new SolidColorBrush(color);
-            yAxis.StrokeThickness = stroke;
-            yAxis.X2 = yAxis.X1 = XPos(0);
-            yAxis.Y1 = YPos(-ymax);
-            yAxis.Y2 = YPos(ymax);
-            c.Children.Add(yAxis);
+            var line = new Line();
+            line.Stroke = new SolidColorBrush(color);
+            line.StrokeThickness = stroke;
+            line.X1 = XPos(x1);
+            line.X2 = XPos(x2);
+            line.Y1 = YPos(y1);
+            line.Y2 = YPos(y2);
+            Debug.WriteLine($"Adding line from {line.X1},{line.Y1} to {line.X2},{line.Y2}");
+            c.Children.Add(line);
         }
 
         private double XPos(int x)
         {
             double size = Math.Min(ActualHeight, ActualWidth);
-            double center = Width / 2;
-            double pos = center + (x * size / 2);
+            double center = ActualWidth / 2;
+            double pos = center + ((x * size) / (2 * xmax));
             return pos;
         }
 
         private double YPos(int y)
         {
-            double size = Math.Min(Height, Width);
-            double center = Height / 2;
-            double pos = center - (y * size / 2);
+            double size = Math.Min(ActualHeight, ActualWidth);
+            double center = ActualHeight / 2;
+            double pos = center - ((y * size) / (2 * ymax));
             return pos;
         }
     }
