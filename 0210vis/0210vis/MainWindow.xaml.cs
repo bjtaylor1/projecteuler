c2c960bce3ec376e7050a015b9f935c8e8d24d9e
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-03 19:19:20 +0000

    210 works

--- 0210vis/0210vis/MainWindow.xaml.cs
+++ 0210vis/0210vis/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace _0210vis
@@ -12,51 +15,120 @@ namespace _0210vis
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        class SaveCanvasCommand : ICommand
         {
-            InitializeComponent();
+            private readonly MainWindow mainWindow;
 
+            public SaveCanvasCommand(MainWindow mainWindow)
+            {
+                this.mainWindow = mainWindow;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                Rect rect = new Rect(mainWindow.c.RenderSize);
+                RenderTargetBitmap rtb = new RenderTargetBitmap((int)rect.Right,
+                  (int)rect.Bottom, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+                rtb.Render(mainWindow.c);
+                //endcode as PNG
+                BitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+                //save to file stream
+                using (var fileStream = File.OpenWrite("output.jpg"))
+                {
+                    encoder.Save(fileStream);
+                }
+            }
         }
 
-        int max = 4;
+        public MainWindow()
+        {
+            SaveCommand = new SaveCanvasCommand(this);
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public int Max
+        {
+            get { return (int)GetValue(maxProperty); }
+            set { SetValue(maxProperty, value); }
+        }
+
+        public static readonly DependencyProperty maxProperty =
+            DependencyProperty.Register("Max", typeof(int), typeof(MainWindow), new PropertyMetadata(4));
+
+
+
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public ICommand SaveCommand { get; }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+
+
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            if (e.Property == maxProperty)
+            {
+                this.InvalidateVisual();
+            }
+        }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             c.Children.Clear();
-            for (int x = -max; x <= max; x++)
-            {
-                AddLine(Brushes.LightGray, 1, x, -max, x, max);
-            }
-            for (int y = -max; y <= max; y++)
-            {
-                AddLine(Brushes.LightGray, 1, -max, y, max, y);
-            }
-            AddLine(Brushes.Black, 2, 0, -max, 0, max); //x axis
-            AddLine(Brushes.Black, 2, -max, 0, max, 0); //y axis
+            //for (int x = -Max; x <= Max; x++)
+            //{
+            //    AddLine(Brushes.LightGray, 1, x, -Max, x, Max);
+            //}
+            //for (int y = -Max; y <= Max; y++)
+            //{
+            //    AddLine(Brushes.LightGray, 1, -Max, y, Max, y);
+            //}
+            AddLine(Brushes.Black, 2, 0, -Max, 0, Max); //x axis
+            AddLine(Brushes.Black, 2, -Max, 0, Max, 0); //y axis
 
-
-            AddLine(Brushes.LightBlue, 1, -max, 0, 0, max);
-            AddLine(Brushes.LightBlue, 1, 0, max, max, 0);
-            AddLine(Brushes.LightBlue, 1, max, 0, 0, -max);
-            AddLine(Brushes.LightBlue, 1, 0, -max, -max, 0);
+            AddLine(Brushes.LightBlue, 1, -Max, 0, 0, Max);
+            AddLine(Brushes.LightBlue, 1, 0, Max, Max, 0);
+            AddLine(Brushes.LightBlue, 1, Max, 0, 0, -Max);
+            AddLine(Brushes.LightBlue, 1, 0, -Max, -Max, 0);
 
             AddPoint(Brushes.Red, 0, 0, 0.5);
-            AddPoint(Brushes.Red, max / 4, max / 4, 0.5);
-
-            for(int x = -max; x <= max; x++)
+            AddPoint(Brushes.Red, Max / 4, Max / 4, 0.5);
+            int numPoints = 0;
+            for (int x = -Max; x <= Max; x++)
             {
-                for(int y = x+1; y <= max; y++)
+                for (int y = x + 1; y <= Max; y++)
                 {
-                    if (Math.Abs(x) + Math.Abs(y) <= max)
+                    if (Math.Abs(x) + Math.Abs(y) <= Max)
                     {
-                        if ((GetAngle(x, y, 0, 0, max / 4, max / 4) > Math.PI / 2)
-                            || (GetAngle(0, 0, x, y, max / 4, max / 4) > Math.PI / 2)
-                            || (GetAngle(max / 4, max / 4, 0, 0, x, y) > Math.PI / 2))
+                        if ((GetAngle(x, y, 0, 0, Max / 4, Max / 4) > Math.PI / 2)
+                            || (GetAngle(0, 0, x, y, Max / 4, Max / 4) > Math.PI / 2)
+                            || (GetAngle(Max / 4, Max / 4, 0, 0, x, y) > Math.PI / 2))
                         {
+                            numPoints++;
                             AddPoint(Brushes.Orange, x, y, 0.25);
                         }
                     }
                 }
             }
+            Message = $"{numPoints} x 2 = {numPoints * 2}";
         }
 
         double GetAngle(double p1x, double p1y, double p2x, double p2y, double p3x, double p3y)
@@ -75,9 +147,8 @@ namespace _0210vis
         private void AddPoint(Brush fill, int x, int y, double boldnessComparedToGridWidth)
         {
             double size = Math.Min(ActualHeight, ActualWidth);
-            double lineX = size / (2 * max);
-            double lineY = size / (2 * max);
-            double shapeSize = Math.Min(lineX, lineY)*boldnessComparedToGridWidth;
+            double line = size / (2 * Max);
+            double shapeSize = line * boldnessComparedToGridWidth;
             var ellipse = new Ellipse();
             ellipse.Height = shapeSize;
             ellipse.Width = shapeSize;
@@ -99,19 +170,19 @@ namespace _0210vis
             c.Children.Add(line);
         }
 
-        private double XPos(int x)
+        private double XPos(double x)
         {
             double size = Math.Min(c.ActualHeight, c.ActualWidth) - 50;
             double center = c.ActualWidth / 2;
-            double pos = center + ((x * size) / (2 * max));
+            double pos = center + ((x * size) / (2 * Max));
             return pos;
         }
 
-        private double YPos(int y)
+        private double YPos(double y)
         {
             double size = Math.Min(c.ActualHeight, c.ActualWidth) - 50;
             double center = c.ActualHeight / 2;
-            double pos = center - ((y * size) / (2 * max));
+            double pos = center - ((y * size) / (2 * Max));
             return pos;
         }
     }
---
+++ 0210vis/0210vis/SaveCanvasCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace _0210vis
+{
+}
