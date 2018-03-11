bfbec664174167d6e6c2b49686a2f131b0beda02
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-11 16:18:24 +0000

    0213cs start

---
+++ 0213cs/0213cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0213cs
+{
+    class Program
+    {
+        ConcurrentDictionary<((int X, int Y) s, int f, int i), double> pcache = new ConcurrentDictionary<((int X, int Y) s, int f, int i), double>();
+        double P((int X, int Y) s, int f, int i) => pcache.GetOrAdd((s, f, i), P);
+        double P(((int X, int Y) s, int f, int i) p)
+        {
+
+        }
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
