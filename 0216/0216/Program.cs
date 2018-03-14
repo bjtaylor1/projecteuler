0b3819ccff9dd5cf8266323beb0b5ef11ef65cbb
Ben Taylor <bentaylortheonly@gmail.com>
2018-03-14 15:16:46 +0000

    216

---
+++ 0216/0216/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mpir.NET;
+
+namespace _0216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tot = 0;
+            for(mpz_t i = 0; i <= 50000000; i++)
+            {
+                if (i.Mod(10000) == 0) Console.Write($"{(int)i:#,##0}   \r");
+                var tn = 2 * i.Power(2) - 1;
+                if (tn.IsProbablyPrimeRabinMiller(10))
+                    tot++;
+            }
+            Console.WriteLine();
+            Console.WriteLine(tot);
+        }
+    }
+}
