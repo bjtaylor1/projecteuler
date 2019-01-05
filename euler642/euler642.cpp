94efde5f19c759c40e42eb2fc237566cd34b810c
Ben Taylor <bentaylortheonly@gmail.com>
2019-01-05 21:01:53 +0000

    642 right for 100 but wrong for 10000

--- euler642/euler642.cpp
+++ euler642/euler642.cpp
@@ -38,21 +38,21 @@ mpz_class sum_of_primes(const mpz_class& n)
     return S[n];
 }
 
-void count_psmooth_r(const mpz_class& n, const mpz_class& p, const mpz_class& prod, mpz_class& tot, int f)
+bool count_psmooth_r(const mpz_class& n, const mpz_class& p, const mpz_class& prod, mpz_class& tot, int f)
 {
     //e.g.                          100                   2                   2                50       -1
     auto amount = n / prod;
     if (amount > 0)
     {
-        
         tot += f * amount;
         for (mpz_class np = mpzfuncs::nextprime(p); np < n; np = mpzfuncs::nextprime(np))
         {
             mpz_class nprod = np * p;
-            count_psmooth_r(n, np, np * prod, tot, f * -1);
-
+            if (!count_psmooth_r(n, np, np * prod, tot, f * -1)) break;
         }
+        return true;
     }
+    else return false;
 }
 
 std::map<mpz_class, mpz_class> psmoothcache;
@@ -73,41 +73,31 @@ mpz_class count_psmooth(const mpz_class& p, const mpz_class n)
 
 int main()
 {
-    long N(100);
+    long N(100000);
     mpz_class r = mpzfuncs::sqrt(N);
     mpz_class tot(0);
+    std::cout << "r = " << r << std::endl;
+    std::cout << "small:" << std::endl;
     for (mpz_class small = 2; small <= r; small = mpzfuncs::nextprime(small))
     {
-        auto occurrences = count_psmooth(small, N);
+        mpz_class occurrences = count_psmooth(small, N);
+        std::cout << small << "\r";
         tot += small * occurrences;
     }
 
-    long lbound = mpzfuncs::nextprime(r).get_ui();
+    std::cout << std::endl << "large:" << std::endl;
+
+    auto lbound = mpzfuncs::nextprime(r);
     while(lbound <= N)
     {
-        auto occurrences = N / lbound; // it's rounded off
-        auto ubound = mpzfuncs::nextprime(N / occurrences).get_ui();  // it's rounded off - so it's not the same as lbound
-        auto totprimes = sum_of_primes(ubound).get_ui() - sum_of_primes(lbound).get_ui() + lbound - ubound;
-        auto subtot = totprimes * occurrences;
+        std::cout << lbound << "\r";
+        mpz_class occurrences = N / lbound; // it's rounded off
+        mpz_class ubound = mpzfuncs::nextprime(N / occurrences);  // it's rounded off - so it's not the same as lbound
+        mpz_class totprimes = sum_of_primes(ubound) - sum_of_primes(lbound) + lbound - ubound;
+        mpz_class subtot = totprimes * occurrences;
         tot += subtot;
         lbound = ubound;
     }
     
-    std::cout << tot << std::endl;
-    //
-    //gmp_randinit_default(randstate);
-
-    //mpz_class p(2);
-    //long long i = 0;
-    //while(p*p < N)
-    //{
-    //    if(i++ % 1000 == 0) std::cout << p << std::endl;
-    //    mpz_class np;
-    //    mpz_next_prime_candidate(np.get_mpz_t(), p.get_mpz_t(), randstate);
-    //    p = np;
-    //}
-
-    
-    //auto ans = sum_of_primes(100);
-    //std::cout << ans << std::endl;
+    std::cout << std::endl << tot << std::endl;
 }
