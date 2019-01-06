f7e80dc9309fd3e8f7498f54375bdc2eafc17ba4
Ben Taylor <bentaylortheonly@gmail.com>
2019-01-06 19:15:42 +0000

    threadedaggregator

--- EulerHelpers/threadedaggregator.h
+++ EulerHelpers/threadedaggregator.h
@@ -8,35 +8,38 @@ template<class TRes, class TParam>
 class threadedaggregator
 {
 private:
-    const int numThreads = 4;
+    const int numThreads = 5;
     std::mutex mutex;
-public:
     void calculate_component_internal(const TParam& lowerboundinc, const TParam& upperboundexc)
     {
         TRes component = calculate_component(lowerboundinc, upperboundexc);
         std::lock_guard<std::mutex> lock(mutex);
         result += component;
     }
+public:
+    threadedaggregator() : result(0) {}
     TRes result;
     virtual TRes calculate_component(const TParam& lowerboundinc, const TParam& upperboundexc) = 0;
     
     void calculate(const TParam& lowerboundinc, const TParam& upperboundexc)
     {
-        std::vector<std::thread> threads;
-        TParam count_per_thread = upperboundexc - lowerboundinc;
+        std::vector<std::thread*> threads;
+        TParam count_per_thread = (upperboundexc - lowerboundinc) / numThreads;
         TParam threadlowerboundinc = lowerboundinc;
         for (TParam  threadupperboundexc = threadlowerboundinc + count_per_thread;
             threadupperboundexc <= upperboundexc;
             threadupperboundexc = threadlowerboundinc + count_per_thread)
         {
-            std::thread t(&threadedaggregator::calculate_component, this, lowerboundinc, upperboundexc);
+            if (threadupperboundexc > upperboundexc) threadupperboundexc = upperboundexc;
+            std::thread* t = new std::thread(&threadedaggregator::calculate_component_internal, this, threadlowerboundinc, threadupperboundexc);
             threads.push_back(t);
             threadlowerboundinc = threadupperboundexc;
         }
 
-        for (std::vector<std::thread>::iterator it = threads.begin(); it != threads.end(); it++)
+        for (auto thread : threads)
         {
-            it->join();
+            thread->join();
+            delete thread;
         }
     }
     virtual ~threadedaggregator() {}
